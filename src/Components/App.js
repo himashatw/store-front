072fc9d9dc3c630f593e65d59a5d96a106390dd0
himashatw <himashatw@gmail.com>
2021-05-08 17:01:45 +0530

    react-router,payment component added

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,30 @@
+import React from 'react'
+import { Route, BrowserRouter as Router, Switch, Link } from 'react-router-dom'
+import Payment from './Payment/Payement';
+import HomePage from './HomePage/HomePage'
 
 function App() {
   return (
-    <div>
-      <p>Hello world</p>
-      <p>Hello world</p>
-    </div>
+    <Router>
+
+      <div className="main-container">
+        <Switch>
+          <Route exact path="/" component={HomePage}></Route>
+          <Route exact path="/payment" component={Payment}></Route>
+        </Switch>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Homepage</Link>
+            </li>
+            <li>
+              <Link to="/payment">Payment</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+    </Router>
   );
 }
 
---
+++ src/Components/Payment/Payement.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Stripe from 'react-stripe-checkout'
+import axios from 'axios'
+
+const Payment = () => {
+
+    async function handleToken(token) {
+        console.log(token); await axios.post("http://localhost:8080/api/payment/charge", "", {
+            headers: {
+                token: token.id,
+                amount: 500,
+            },
+        }).then(() => {
+            alert("Payment Success");
+        }).catch((error) => {
+            alert(error);
+        });
+    }
+
+    return (
+        <div className="App">
+            <Stripe
+                stripeKey="pk_test_51IoQ2qLTLTjtyCAiMJOoTxiymEs2tXlKs1dylU2TnrNfcGGGk5lqssx5FUezwa4F3uWfiYzgBUZ4Cg9CuQMec0QW00gBzPGthB" token={handleToken} />
+        </div>
+    )
+}
+
+export default Payment;
